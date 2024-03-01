5004b7746726b64f426accd1fc18b802c442ae0a
ggoma <sungwoopark95@gmail.com>
2024-03-02 03:10:23 +0900

    add og

--- site/vocs.config.ts
+++ site/vocs.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vocs'
 import { sidebar } from './docs/sidebar'
 
 export default defineConfig({
-  title: 'Mint.club',
+  ogImageUrl: 'https://vocs.dev/api/og?logo=%logo&title=%title&description=%description',
+  title: 'Mint.club V2 SDK Documentation',
+  description:
+    'Create a custom bonding curve token/NFT on mint.club using your preferred ERC20, instantly tradable & monetizable.',
   logoUrl: 'https://mint.club/assets/icons/mint-logo.png',
   sidebar,
   theme: {
