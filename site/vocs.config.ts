7aebc5928d76fdbed94ae32066ae6dde02051933
ggoma <sungwoopark95@gmail.com>
2024-03-02 03:23:58 +0900

    Update Mint Club V2 SDK documentation

--- site/vocs.config.ts
+++ site/vocs.config.ts
@@ -4,8 +4,7 @@ import { sidebar } from './docs/sidebar'
 export default defineConfig({
   ogImageUrl: 'https://vocs.dev/api/og?logo=%logo&title=%title&description=%description',
   title: 'Mint.club V2 SDK Documentation',
-  description:
-    'Create a custom bonding curve token/NFT on mint.club using your preferred ERC20, instantly tradable & monetizable.',
+  description: 'Effortlessly Create Bonding Curved Tokens',
   logoUrl: 'https://mint.club/assets/icons/mint-logo.png',
   sidebar,
   theme: {
