9a45b04b0b5820c45db19be3a65d72ac218fff19
ggoma <sungwoopark95@gmail.com>
2024-03-01 22:03:33 +0900

    update docs

--- site/docs/sidebar.ts
+++ site/docs/sidebar.ts
@@ -7,25 +7,25 @@ export const sidebar = {
       items: [
         {
           text: 'Why Mint.club?',
-          link: '/docs/why-mint-club'
+          link: '/docs/why-mint-club',
         },
         {
           text: 'Getting Started',
-          link: '/docs/getting-started'
+          link: '/docs/getting-started',
         },
         {
           text: 'Contracts',
-          link: '/docs/contracts'
+          link: '/docs/contracts',
         },
         {
           text: 'Curve Design',
-          link: '/docs/curve-design'
+          link: '/docs/curve-design',
         },
         {
           text: 'FAQ',
-          link: '/docs/faq'
-        }
-      ]
+          link: '/docs/faq',
+        },
+      ],
     },
 
     {
@@ -48,66 +48,102 @@ export const sidebar = {
                   items: [
                     {
                       text: 'create',
-                      link: '/docs/sdk/network/token/create'
+                      link: '/docs/sdk/network/token/create',
                     },
                     {
                       text: 'getAllowance',
-                      link: '/docs/sdk/network/token/getAllowance'
+                      link: '/docs/sdk/network/token/getAllowance',
                     },
                     {
                       text: 'getBalanceOf',
-                      link: '/docs/sdk/network/token/getBalanceOf'
+                      link: '/docs/sdk/network/token/getBalanceOf',
                     },
                     {
-                      text: 'getGetBondAddress',
-                      link: '/docs/sdk/network/token/getGetBondAddress'
+                      text: 'getBondAddress',
+                      link: '/docs/sdk/network/token/getBondAddress',
                     },
                     {
                       text: 'getDecimals',
-                      link: '/docs/sdk/network/token/getDecimals'
+                      link: '/docs/sdk/network/token/getDecimals',
                     },
                     {
                       text: 'getName',
-                      link: '/docs/sdk/network/token/getName'
+                      link: '/docs/sdk/network/token/getName',
                     },
                     {
                       text: 'getSymbol',
-                      link: '/docs/sdk/network/token/getSymbol'
+                      link: '/docs/sdk/network/token/getSymbol',
                     },
                     {
                       text: 'getTotalSupply',
-                      link: '/docs/sdk/network/token/getTotalSupply'
-                    }
-                  ]
+                      link: '/docs/sdk/network/token/getTotalSupply',
+                    },
+                  ],
                 },
                 {
                   text: 'nft',
                   collapsed: false,
                   items: [
                     {
-                      text: 'createToken',
-                      link: '/docs/sdk/network/getBalance'
+                      text: 'create',
+                      link: '/docs/sdk/network/nft/create',
                     },
                     {
-                      text: 'createNFT',
-                      link: '/docs/sdk/network/getBalance'
-                    }
-                  ]
+                      text: 'getBalanceOf',
+                      link: '/docs/sdk/network/nft/getBalanceOf',
+                    },
+                    {
+                      text: 'getBalanceOfBatch',
+                      link: '/docs/sdk/network/nft/getBalanceOfBatch',
+                    },
+                    {
+                      text: 'getContrctURI',
+                      link: '/docs/sdk/network/nft/getContrctURI',
+                    },
+                    {
+                      text: 'getDecimals',
+                      link: '/docs/sdk/network/nft/getDecimals',
+                    },
+                    {
+                      text: 'getIsApprovedForAll',
+                      link: '/docs/sdk/network/nft/getIsApprovedForAll',
+                    },
+                    {
+                      text: 'getName',
+                      link: '/docs/sdk/network/nft/getName',
+                    },
+                    {
+                      text: 'getSupportsInterface',
+                      link: '/docs/sdk/network/nft/getSupportsInterface',
+                    },
+                    {
+                      text: 'getSymbol',
+                      link: '/docs/sdk/network/nft/getSymbol',
+                    },
+                    {
+                      text: 'getTotalSupply',
+                      link: '/docs/sdk/network/nft/getTotalSupply',
+                    },
+                    {
+                      text: 'getUri',
+                      link: '/docs/sdk/network/nft/getUri',
+                    },
+                  ],
                 },
                 {
                   text: 'bond',
                   collapsed: true,
-                  link: '/docs/sdk/network/getBalance'
+                  link: '/docs/sdk/network/getBalance',
                 },
                 {
                   text: 'airdrop',
                   collapsed: true,
-                  link: '/docs/sdk/network/getBalance'
+                  link: '/docs/sdk/network/getBalance',
                 },
                 {
                   text: 'lockup',
                   collapsed: true,
-                  link: '/docs/sdk/network/getBalance'
+                  link: '/docs/sdk/network/getBalance',
                 },
                 {
                   text: 'Making Write Calls',
@@ -116,37 +152,37 @@ export const sidebar = {
                   items: [
                     {
                       text: 'getConnectedAddress',
-                      link: '/docs/sdk/network/write/getConnectedAddress'
+                      link: '/docs/sdk/network/write/getConnectedAddress',
                     },
                     {
                       text: 'getWalletClient',
-                      link: '/docs/sdk/network/write/getWalletClient'
+                      link: '/docs/sdk/network/write/getWalletClient',
                     },
                     {
                       text: 'getPublicClient',
-                      link: '/docs/sdk/network/write/getPublicClient'
+                      link: '/docs/sdk/network/write/getPublicClient',
                     },
                     {
                       text: 'withConfig',
-                      link: '/docs/sdk/network/write/withConfig'
+                      link: '/docs/sdk/network/write/withConfig',
                     },
                     {
                       text: 'withPrivateKey',
-                      link: '/docs/sdk/network/write/withPrivateKey'
+                      link: '/docs/sdk/network/write/withPrivateKey',
                     },
                     {
                       text: 'withAccount',
-                      link: '/docs/sdk/network/write/withAccount'
+                      link: '/docs/sdk/network/write/withAccount',
                     },
                     {
                       text: 'withProvider',
-                      link: '/docs/sdk/network/write/withProvider'
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
+                      link: '/docs/sdk/network/write/withProvider',
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
         },
 
         {
@@ -155,15 +191,15 @@ export const sidebar = {
           items: [
             {
               text: 'a',
-              link: '/docs/sdk/network/getBalance'
+              link: '/docs/sdk/network/getBalance',
             },
             {
               text: 'b',
-              link: '/docs/sdk/network/getBalance'
-            }
-          ]
-        }
-      ]
-    }
-  ]
+              link: '/docs/sdk/network/getBalance',
+            },
+          ],
+        },
+      ],
+    },
+  ],
 } as const satisfies Sidebar
