95269d248b852673e476bbd2917eab571fec5c3c
ggoma <sungwoopark95@gmail.com>
2024-03-03 23:28:59 +0900

    Remove unused vocs.config.ts file

--- site/vocs.config.tsx
+++ site/vocs.config.tsx
@@ -1,7 +1,17 @@
+import * as React from 'react'
 import { defineConfig } from 'vocs'
 import { sidebar } from './docs/sidebar'
 
 export default defineConfig({
+  head: (
+    <>
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Mint.club V2 SDK Documentation" />
+      <meta property="og:image" content="https://vocs.dev/api/og?logo=%logo&title=%title&description=%description" />
+      <meta property="og:url" content="https://sdk.mint.club" />
+      <meta property="og:description" content="Create and Trade Bonding Curve Tokens / NFTs with Automated Liquidity" />
+    </>
+  ),
   ogImageUrl: 'https://vocs.dev/api/og?logo=%logo&title=%title&description=%description',
   title: 'Mint.club V2 SDK Documentation',
   description: 'Create and Trade Bonding Curve Tokens / NFTs with Automated Liquidity',
