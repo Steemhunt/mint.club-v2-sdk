2e10ae699dab56a002a3f9e552b53d45d217b8c5
ggoma <sungwoopark95@gmail.com>
2024-03-03 23:14:28 +0900

    Add emojis and restructure sdk docs

--- site/docs/sidebar.ts
+++ site/docs/sidebar.ts
@@ -29,197 +29,191 @@ export const sidebar = {
     },
 
     {
-      text: 'SDK',
+      text: 'Mint.club v2 SDK 📦',
       items: [
         {
-          text: 'mintclub',
+          text: '️🚩 Start here',
+          link: '/docs/sdk/introduction',
+        },
+        {
+          text: '🌐 network',
+          link: '/docs/sdk/network',
+          collapsed: false,
+        },
+        {
+          text: '🪙 token',
+          link: '/docs/sdk/network/token',
           collapsed: false,
           items: [
             {
-              text: 'Introduction',
-              link: '/docs/sdk/introduction',
+              text: 'create',
+              link: '/docs/sdk/network/token/create',
             },
             {
-              text: 'network',
-              link: '/docs/sdk/network',
-              collapsed: false,
-              items: [
-                {
-                  text: 'token',
-                  link: '/docs/sdk/network/token',
-                  collapsed: false,
-                  items: [
-                    {
-                      text: 'create',
-                      link: '/docs/sdk/network/token/create',
-                    },
-                    {
-                      text: 'approve',
-                      link: '/docs/sdk/network/token/approve',
-                    },
-                    {
-                      text: 'buy',
-                      link: '/docs/sdk/network/token/buy',
-                    },
-                    {
-                      text: 'transfer',
-                      link: '/docs/sdk/network/token/transfer',
-                    },
-                    {
-                      text: 'getPriceForNextMint',
-                      link: '/docs/sdk/network/token/getPriceForNextMint',
-                    },
-                    {
-                      text: 'getAllowance',
-                      link: '/docs/sdk/network/token/getAllowance',
-                    },
-                    {
-                      text: 'getBalanceOf',
-                      link: '/docs/sdk/network/token/getBalanceOf',
-                    },
-                    {
-                      text: 'getBondAddress',
-                      link: '/docs/sdk/network/token/getBondAddress',
-                    },
-                    {
-                      text: 'getDecimals',
-                      link: '/docs/sdk/network/token/getDecimals',
-                    },
-                    {
-                      text: 'getName',
-                      link: '/docs/sdk/network/token/getName',
-                    },
-                    {
-                      text: 'getSymbol',
-                      link: '/docs/sdk/network/token/getSymbol',
-                    },
-                    {
-                      text: 'getTotalSupply',
-                      link: '/docs/sdk/network/token/getTotalSupply',
-                    },
-                  ],
-                },
-                {
-                  text: 'nft',
-                  link: '/docs/sdk/network/nft',
-                  collapsed: true,
-                  items: [
-                    {
-                      text: 'create',
-                      link: '/docs/sdk/network/nft/create',
-                    },
-                    {
-                      text: 'getBalanceOf',
-                      link: '/docs/sdk/network/nft/getBalanceOf',
-                    },
-                    {
-                      text: 'getBalanceOfBatch',
-                      link: '/docs/sdk/network/nft/getBalanceOfBatch',
-                    },
-                    {
-                      text: 'getContrctURI',
-                      link: '/docs/sdk/network/nft/getContractURI',
-                    },
-                    {
-                      text: 'getDecimals',
-                      link: '/docs/sdk/network/nft/getDecimals',
-                    },
-                    {
-                      text: 'getIsApprovedForAll',
-                      link: '/docs/sdk/network/nft/getIsApprovedForAll',
-                    },
-                    {
-                      text: 'getName',
-                      link: '/docs/sdk/network/nft/getName',
-                    },
-                    {
-                      text: 'getSupportsInterface',
-                      link: '/docs/sdk/network/nft/getSupportsInterface',
-                    },
-                    {
-                      text: 'getSymbol',
-                      link: '/docs/sdk/network/nft/getSymbol',
-                    },
-                    {
-                      text: 'getTotalSupply',
-                      link: '/docs/sdk/network/nft/getTotalSupply',
-                    },
-                    {
-                      text: 'getUri',
-                      link: '/docs/sdk/network/nft/getUri',
-                    },
-                  ],
-                },
-                {
-                  text: 'bond',
-                  collapsed: true,
-                  items: [
-                    {
-                      text: 'getCreationFee',
-                      link: '/docs/sdk/network/bond/getCreationFee',
-                    },
-                  ],
-                },
-                {
-                  text: 'airdrop',
-                  collapsed: true,
-                  link: '/docs/sdk/network/airdrop',
-                },
-                {
-                  text: 'lockup',
-                  collapsed: true,
-                  link: '/docs/sdk/network/lockup',
-                },
-                {
-                  text: 'Transactions',
-                  collapsed: true,
-                  items: [
-                    {
-                      text: 'Introduction',
-                      link: '/docs/sdk/network/transactions',
-                    },
-                    {
-                      text: 'getPublicClient',
-                      link: '/docs/sdk/network/transactions/getPublicClient',
-                    },
-                    {
-                      text: 'getWalletClient',
-                      link: '/docs/sdk/network/transactions/getWalletClient',
-                    },
-                    {
-                      text: 'withAccount',
-                      link: '/docs/sdk/network/transactions/withAccount',
-                    },
-                    {
-                      text: 'withPublicClient',
-                      link: '/docs/sdk/network/transactions/withPublicClient',
-                    },
-                    {
-                      text: 'withWalletClient',
-                      link: '/docs/sdk/network/transactions/withWalletClient',
-                    },
-                    {
-                      text: 'withPrivateKey',
-                      link: '/docs/sdk/network/transactions/withPrivateKey',
-                    },
-                    {
-                      text: 'withProvider',
-                      link: '/docs/sdk/network/transactions/withProvider',
-                    },
-                    {
-                      text: 'TransactionReceipt',
-                      link: '/docs/sdk/network/transactions/receipt',
-                    },
-                    {
-                      text: 'RPCs',
-                      link: '/docs/sdk/network/transactions/rpcs',
-                    },
-                  ],
-                },
-              ],
+              text: 'approve',
+              link: '/docs/sdk/network/token/approve',
+            },
+            {
+              text: 'buy',
+              link: '/docs/sdk/network/token/buy',
+            },
+            {
+              text: 'transfer',
+              link: '/docs/sdk/network/token/transfer',
+            },
+            {
+              text: 'getPriceForNextMint',
+              link: '/docs/sdk/network/token/getPriceForNextMint',
+            },
+            {
+              text: 'getAllowance',
+              link: '/docs/sdk/network/token/getAllowance',
+            },
+            {
+              text: 'getBalanceOf',
+              link: '/docs/sdk/network/token/getBalanceOf',
+            },
+            {
+              text: 'getBondAddress',
+              link: '/docs/sdk/network/token/getBondAddress',
+            },
+            {
+              text: 'getDecimals',
+              link: '/docs/sdk/network/token/getDecimals',
+            },
+            {
+              text: 'getName',
+              link: '/docs/sdk/network/token/getName',
+            },
+            {
+              text: 'getSymbol',
+              link: '/docs/sdk/network/token/getSymbol',
+            },
+            {
+              text: 'getTotalSupply',
+              link: '/docs/sdk/network/token/getTotalSupply',
             },
           ],
         },
+        {
+          text: '🖼️ nft',
+          link: '/docs/sdk/network/nft',
+          collapsed: true,
+          items: [
+            {
+              text: 'create',
+              link: '/docs/sdk/network/nft/create',
+            },
+            {
+              text: 'getBalanceOf',
+              link: '/docs/sdk/network/nft/getBalanceOf',
+            },
+            {
+              text: 'getBalanceOfBatch',
+              link: '/docs/sdk/network/nft/getBalanceOfBatch',
+            },
+            {
+              text: 'getContractURI',
+              link: '/docs/sdk/network/nft/getContractURI',
+            },
+            {
+              text: 'getDecimals',
+              link: '/docs/sdk/network/nft/getDecimals',
+            },
+            {
+              text: 'getIsApprovedForAll',
+              link: '/docs/sdk/network/nft/getIsApprovedForAll',
+            },
+            {
+              text: 'getName',
+              link: '/docs/sdk/network/nft/getName',
+            },
+            {
+              text: 'getSupportsInterface',
+              link: '/docs/sdk/network/nft/getSupportsInterface',
+            },
+            {
+              text: 'getSymbol',
+              link: '/docs/sdk/network/nft/getSymbol',
+            },
+            {
+              text: 'getTotalSupply',
+              link: '/docs/sdk/network/nft/getTotalSupply',
+            },
+            {
+              text: 'getUri',
+              link: '/docs/sdk/network/nft/getUri',
+            },
+          ],
+        },
+        {
+          text: '📈 bond',
+          collapsed: true,
+          items: [
+            {
+              text: 'getCreationFee',
+              link: '/docs/sdk/network/bond/getCreationFee',
+            },
+          ],
+        },
+        {
+          text: '🪂 airdrop',
+          link: '/docs/sdk/network/airdrop',
+          collapsed: true,
+        },
+        {
+          text: '🔒lockup',
+          link: '/docs/sdk/network/lockup',
+          collapsed: true,
+        },
+        {
+          text: '📝 Transactions',
+          link: '/docs/sdk/network/transactions',
+          collapsed: true,
+          items: [
+            {
+              text: 'Introduction',
+              link: '/docs/sdk/network/transactions',
+            },
+            {
+              text: 'getPublicClient',
+              link: '/docs/sdk/network/transactions/getPublicClient',
+            },
+            {
+              text: 'getWalletClient',
+              link: '/docs/sdk/network/transactions/getWalletClient',
+            },
+            {
+              text: 'withAccount',
+              link: '/docs/sdk/network/transactions/withAccount',
+            },
+            {
+              text: 'withPublicClient',
+              link: '/docs/sdk/network/transactions/withPublicClient',
+            },
+            {
+              text: 'withWalletClient',
+              link: '/docs/sdk/network/transactions/withWalletClient',
+            },
+            {
+              text: 'withPrivateKey',
+              link: '/docs/sdk/network/transactions/withPrivateKey',
+            },
+            {
+              text: 'withProvider',
+              link: '/docs/sdk/network/transactions/withProvider',
+            },
+            {
+              text: 'TransactionReceipt',
+              link: '/docs/sdk/network/transactions/receipt',
+            },
+            {
+              text: 'RPCs',
+              link: '/docs/sdk/network/transactions/rpcs',
+            },
+          ],
+        },
+        // Any additional items or categories should follow the same pattern
       ],
     },
   ],
