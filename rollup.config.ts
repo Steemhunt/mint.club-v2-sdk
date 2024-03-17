939d1d5bea2e08d5934d4a5d9b1bd07aedfacb41
ggoma <sungwoopark95@gmail.com>
2024-03-17 20:39:56 +0900

    edit docs for lockup and airdrop

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,4 +1,5 @@
 import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
 
 const config = [
   {
@@ -19,6 +20,28 @@ const config = [
         tsconfig: 'tsconfig.json',
         exclude: ['**/test', '**/*.test.ts', '/examples'],
       }),
+      terser(),
+    ],
+  },
+  {
+    input: 'src/node.ts',
+    output: [
+      {
+        file: 'dist/cjs/node.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/esm/node.js',
+        format: 'es',
+      },
+    ],
+    plugins: [
+      typescript({
+        module: 'ESNext',
+        tsconfig: 'tsconfig.json',
+        exclude: ['**/test', '**/*.test.ts', '/examples'],
+      }),
+      terser(),
     ],
   },
 ];
