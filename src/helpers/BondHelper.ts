d94e41f89069e04e259fb5902183e1c67796a0e8
0xggoma <sungwoopark95@gmail.com>
2024-03-18 15:28:25 +0900

    update docs

--- src/helpers/BondHelper.ts
+++ src/helpers/BondHelper.ts
@@ -14,17 +14,19 @@ export class Bond {
     });
   }
 
-  public getTokensByReserveToken(reserveToken: `0x${string}`) {
+  public getTokensByReserveToken(params: { reserveToken: `0x${string}`; start?: number; end?: number }) {
+    const { reserveToken, start = 0, end = 1000 } = params;
     return bondContract.network(this.chainId).read({
       functionName: 'getTokensByReserveToken',
-      args: [reserveToken, 0n, 1000n],
+      args: [reserveToken, BigInt(start), BigInt(end)],
     });
   }
 
-  public getTokensByCreator(reserveToken: `0x${string}`) {
+  public getTokensByCreator(params: { creator: `0x${string}`; start?: number; end?: number }) {
+    const { creator, start = 0, end = 1000 } = params;
     return bondContract.network(this.chainId).read({
       functionName: 'getTokensByCreator',
-      args: [reserveToken, 0n, 1000n],
+      args: [creator, BigInt(start), BigInt(end)],
     });
   }
 }
