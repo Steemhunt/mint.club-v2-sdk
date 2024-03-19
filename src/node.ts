0bebd0f450010ac246f3c528b14e22e130be0076
ggoma <sungwoopark95@gmail.com>
2024-03-19 17:16:30 +0900

    pass url in jsdom

--- src/node.ts
+++ src/node.ts
@@ -3,7 +3,9 @@ export * from 'viem';
 export type * from 'viem';
 if (typeof window === 'undefined') {
   const JSDOM = jsdom.JSDOM;
-  const { window } = new JSDOM('');
+  const { window } = new JSDOM('', {
+    url: 'https://mint.club',
+  });
   (global.window as any) = window;
   global.document = window.document;
 }
