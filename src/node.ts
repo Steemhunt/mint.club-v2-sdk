c1cb60394905832da1f940f16cc962eb9fe9530b
0xggoma <sungwoopark95@gmail.com>
2024-04-02 02:23:51 +0900

    jsdom

--- src/node.ts
+++ src/node.ts
@@ -1,13 +1,14 @@
-import jsdom from 'jsdom';
 export * from 'viem';
 export type * from 'viem';
 if (typeof window === 'undefined') {
-  const JSDOM = jsdom.JSDOM;
-  const { window } = new JSDOM('', {
-    url: 'https://mint.club',
+  import('jsdom').then((jsdom) => {
+    const JSDOM = jsdom.JSDOM;
+    const { window } = new JSDOM('', {
+      url: 'https://mint.club',
+    });
+    (global.window as any) = window;
+    global.document = window.document;
   });
-  (global.window as any) = window;
-  global.document = window.document;
 }
 
 export * from './index';
