7b9b5230882aca4003f92423865f396b5c823d77
0xggoma <sungwoopark95@gmail.com>
2024-12-19 18:29:12 -0800

    increase timeout to 10s

--- src/constants/rpcs.ts
+++ src/constants/rpcs.ts
@@ -199,7 +199,7 @@ function customTransport(params: { rpc?: string; fetchOptions?: HttpTransportCon
     name: rpc,
     fetchOptions,
     retryCount: 0,
-    timeout: 4_000,
+    timeout: 10_000,
   });
 }
 
@@ -224,7 +224,7 @@ export const DEFAULT_RANK_OPTIONS: FallbackTransportConfig = {
   rank: {
     interval: 100_000,
     sampleCount: 5,
-    timeout: 4_000,
+    timeout: 10_000,
     weights: {
       latency: 0.3,
       stability: 0.7,
